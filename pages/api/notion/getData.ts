500b4beb2dc9da98ef9b7291464ac3724ee6831d
ugongone <yu1931sn@gmail.com>
2025-01-07 21:45:41 +0900

    Enhance Notion API integration by adding status property and refining type definitions
    - Updated the Notion API handler in pages/api/notion/getData.ts to include a new 'Status' property in the NotionProperties type.
    - Modified the FormattedResponse type to include the 'status' field.
    - Implemented a filter in the Notion database query to retrieve only items with the status 'InProgress'.
    - Improved code formatting for better readability and consistency.

--- pages/api/notion/getData.ts
+++ pages/api/notion/getData.ts
@@ -1,65 +1,86 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { Client } from '@notionhq/client'
-import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
-import { runCorsMiddleware } from '@/utils/cors'
+import { NextApiRequest, NextApiResponse } from "next";
+import { Client } from "@notionhq/client";
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { runCorsMiddleware } from "@/utils/cors";
 
 type NotionProperties = {
-    ID: {
-        unique_id: {
-            prefix: string
-            number: number
-        }
-    }
-    Japanese: {
-        rich_text: Array<{
-            plain_text: string
-        }>
-    }
-    English: {
-        title: Array<{
-            plain_text: string
-        }>
-    }
-    Date: {
-        date: {
-            start: string
-        } | null
-    }
-}
+  ID: {
+    unique_id: {
+      prefix: string;
+      number: number;
+    };
+  };
+  Japanese: {
+    rich_text: Array<{
+      plain_text: string;
+    }>;
+  };
+  English: {
+    title: Array<{
+      plain_text: string;
+    }>;
+  };
+  Date: {
+    date: {
+      start: string;
+    } | null;
+  };
+  Status: {
+    select: {
+      name: string;
+    };
+  };
+};
 
 type FormattedResponse = {
-    id: string
-    japanese: string
-    english: string
-    date: string
-}
+  id: string;
+  japanese: string;
+  english: string;
+  date: string;
+  status: string;
+};
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY })
-const databaseId = process.env.NOTION_DATABASE_ID
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // CORSを許可
-    await runCorsMiddleware(req, res)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // CORSを許可
+  await runCorsMiddleware(req, res);
 
-    try {
-        // Notion からDBのデータを取得
-        const response = await notion.databases.query({
-            database_id: databaseId as string,
-        })
+  try {
+    // Notion からDBのデータを取得
+    const response = await notion.databases.query({
+      database_id: databaseId as string,
+      filter: {
+        property: "Status",
+        select: {
+          equals: "InProgress",
+        },
+      },
+    });
 
-        const formattedResponse: FormattedResponse[] = (response.results as PageObjectResponse[]).map((result: PageObjectResponse) => {
-            const properties = result.properties as unknown as NotionProperties
-            return {
-                id: properties.ID.unique_id.prefix + '-' + properties.ID.unique_id.number,
-                japanese: properties.Japanese.rich_text[0]?.plain_text || '',
-                english: properties.English.title[0]?.plain_text || '',
-                date: properties.Date.date?.start || '',
-            }
-        })
+    const formattedResponse: FormattedResponse[] = (
+      response.results as PageObjectResponse[]
+    ).map((result: PageObjectResponse) => {
+      const properties = result.properties as unknown as NotionProperties;
+      return {
+        id:
+          properties.ID.unique_id.prefix + "-" + properties.ID.unique_id.number,
+        japanese: properties.Japanese.rich_text[0]?.plain_text || "",
+        english: properties.English.title[0]?.plain_text || "",
+        date: properties.Date.date?.start || "",
+        status: properties.Status.select.name,
+      };
+    });
 
-        res.status(200).json({ data: formattedResponse })
-    } catch (error: Error | unknown) {
-        console.error(error)
-        res.status(500).json({ error: error instanceof Error ? error.message : 'Unknown error occurred' })
-    }
+    res.status(200).json({ data: formattedResponse });
+  } catch (error: Error | unknown) {
+    console.error(error);
+    res.status(500).json({
+      error: error instanceof Error ? error.message : "Unknown error occurred",
+    });
+  }
 }
